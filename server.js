f127646f0be36628f2525d4e481773aabcd68f4e
unknown <huutienn46@gmail.com>
2022-05-18 07:43:42 +0700

    first commit

---
+++ server.js
@@ -0,0 +1,83 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
+var cors = require('cors')
+const app = express();
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cors())
+ app.listen(80, function () {
+  console.log('CORS-enabled web server listening on port 80')
+})
+const port = 3000;
+const dbUri =
+  'mongodb+srv://clone:clone@twitterclonecluster.cdhdf.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
+mongoose
+  .connect(dbUri)
+  .then(() => {
+    console.log('Database connection successful!');
+  })
+  .catch((err) => {
+    console.log('Database connection error: ' + err);
+  });
+const Schema = mongoose.Schema;
+
+const UserSchema = new Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+      trim: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const User = mongoose.model('UserNew', UserSchema);
+
+// get all user
+app.get('/users', async (req, res, next) => {
+  const users = await User.find();
+  res.status(200).json(users);
+});
+
+// login
+app.post('/login', async (req, res, next) => {
+  const payload = req.body;
+  const { username, password } = payload;
+  if (!username || !password) return res.sendStatus(500);
+  const currentUser = await User.findOne({
+    username,
+  });
+  if (!currentUser) return res.status(400).json({ error: 'User not found' });
+  const isValidPassword = await bcrypt.compare(password, currentUser.password);
+  if (!isValidPassword)
+    return res.status(300).json({ error: 'Valid password fail' });
+  res.json(currentUser);
+});
+// register
+app.post('/register', async (req, res, next) => {
+  const payload = req.body;
+  const { username, password } = payload;
+  if (!username || !password) return res.sendStatus(500);
+  const currentPassword = await bcrypt.hash(password, 10);
+  const checkUsername = await User.findOne({
+    username,
+  });
+  if (!!checkUsername) return res.sendStatus(500);
+  const currentUser = await User.create({
+    username,
+    password: currentPassword,
+  });
+  if (!currentUser) return res.sendStatus(500);
+  res.json(currentUser);
+});
+
+app.listen(port, () => console.log('Server listening on port ' + port));
